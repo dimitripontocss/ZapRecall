acb68220ab9c617581b6896a6d741773c0074f63
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-05-12 21:27:48 -0300

    Esboço da parte logica de concluir feito

--- src/MenuContent.js
+++ src/MenuContent.js
@@ -4,20 +4,32 @@ import curvedArrow from "./assets/setinha.png"
 export default function MenuContent({card, counter, setCounter}){
 const [opened,setOpened] = React.useState(null)
 const [fliped, setFliped] = React.useState(null)
+const [completed, setCompleted] = React.useState([])
+    
     function open(index){
-        setCounter(counter +1)
         setOpened(index)
+        setFliped(null)
     }
     function reveal(index){
         setFliped(index)
     }
-
+    function complete(index, code){
+        const auxComplete = [...completed, {pos:index, code:code}]
+        setCompleted(auxComplete)
+    }
+    console.log(completed)
     return(
         <div className="Content">
             
             {card.map((value,index) =>
-                index !== opened ? <DefaultCard index={index} open={open}/> 
-             : <OpenedCard card ={card} index={index} reveal={reveal} fliped={fliped}/>
+                {if(index !== opened){
+                    
+                  return  <DefaultCard index={index} open={open}/>
+                    
+                }else{
+                    return <OpenedCard card ={card} index={index} reveal={reveal} fliped={fliped} complete={complete}/>
+                }}
+                
              )
             }
             </div>
@@ -33,7 +45,7 @@ function DefaultCard({index, open}){
     )
 }
 
-function OpenedCard({card, index, reveal, fliped}){
+function OpenedCard({card, index, reveal, fliped ,complete}){
     return(
         fliped !== index ?
         <div key={index} className="open-card">
@@ -42,8 +54,13 @@ function OpenedCard({card, index, reveal, fliped}){
         </div> 
         : 
         <div key={index} className="revealed-card">
-            <h4>{card[index].answer}</h4>
-            <ion-icon onClick={()=>reveal(index)} name="play-outline"></ion-icon>
+            <div><h4>{card[index].answer}</h4></div>
+            <div className="box">
+                <div onClick={()=>complete(index, "erro")} className="erro">Não lembrei</div>
+                <div onClick={()=>complete(index, "quase")} className="quase">Quase não lembrei</div>
+                <div onClick={()=>complete(index, "zap")} className="zap">Zap!</div>
+            </div>
+            
         </div>
 
     )
