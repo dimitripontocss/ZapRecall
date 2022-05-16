8fb8556ee90f39099a599c2c6ff54f02c1ccb5d3
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-05-16 00:03:06 -0300

    Projeto finalizado

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import './css/style.css'
 
 export default function App(){
     const [display, setDisplay] = React.useState(true);
-    const [target,setTarget] = React.useState("")
-    const [which,setWhich] = React.useState("")
-    
-    let card = [
+    const [target,setTarget] = React.useState("");
+    const [which,setWhich] = React.useState({value: "JavaScript"});
+
+    let card = [];
+
+    const Deck1 = [
         {
             question:"O que é JSX?",
             answer:"Uma extensão de linguagem do JavaScript"
@@ -44,44 +46,48 @@ export default function App(){
             answer:"dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
         }
     ];
+    const Deck2 = [
+        {
+            question:"Qual meu nome?",
+            answer:"Dimitri"
+        },
+        {
+            question:"Sou natural de __",
+            answer:"Juiz de Fora city" 
+        },
+        {
+            question:"Quem gosta de css é__ ",
+            answer:"dodoi"
+        },
+        {
+            question:"React é__",
+            answer:"bala"
+        },
+        {
+            question:"Sem FNX__",
+            answer:"sem Major"
+        }
+    ];
+
+    const Deck1RND = Deck1.sort(() => {
+        return (Math.random() - 0.5);
+    })
+
+    const Deck2RND = Deck2.sort(() => {
+        return (Math.random() - 0.5);
+    })
+
+    if(!display){
+        selectCard();
+    }
 
     function selectCard(){
-        setDisplay(false);
-        if(which === "JavaScritp"){
-            card = [
-                {
-                    question:"O que é JSX?",
-                    answer:"Uma extensão de linguagem do JavaScript"
-                },
-                {
-                    question:"O React é __",
-                    answer:"uma biblioteca JavaScript para construção de interfaces" 
-                },
-                {
-                    question:"Componentes devem iniciar com __ ",
-                    answer:"letra maiúscula"
-                },
-                {
-                    question:"Podemos colocar __ dentro do JSX",
-                    answer:"expressões"
-                },
-                {
-                    question:"O ReactDOM nos ajuda __",
-                    answer:"interagindo com a DOM para colocar componentes React na mesma"
-                },
-                {
-                    question:"Usamos o npm para __",
-                    answer:"gerenciar os pacotes necessários e suas dependências"
-                },
-                {
-                    question:"Usamos props para __ ",
-                    answer:"passar diferentes informações para componentes"
-                },
-                {
-                    question:"Usamos estado (state) para __",
-                    answer:"dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
-                }
-            ]
+        if(which.value === "JavaScript"){
+            console.log("confirmou")
+            card = Deck1RND;
+
+        }else if(which.value === "Random"){
+            card = Deck2RND;
         }
     }
  
--- src/Inicio.js
+++ src/Inicio.js
@@ -6,19 +6,17 @@ export default function Inicio({setDisplay,target,setTarget,which,setWhich,selec
         color: '#C0C0C0',
         background: '#E8E8E8'
       };
-      console.log(typeof which)
+      console.log(which)
     return(
         <div className="MenuInicial" >
                 <div>
                      <img src={logo} alt="Minha Figura"/>
                      <h1>ZapRecall</h1>
-                     <input type="option" list="cards" 
-                     value={which} 
-                     onChange={(x) => setWhich(x.which.value)}/>
-                        <datalist id="cards">
+                     <select value={which.value} 
+                     onChange={(x) => setWhich({value: x.target.value})}>
                         <option>JavaScript</option>
-                        <option>Wilian</option>
-                        </datalist>
+                        <option>Random</option>
+                        </select>
                      <input
                         type="text" 
                         value={target}
@@ -26,7 +24,7 @@ export default function Inicio({setDisplay,target,setTarget,which,setWhich,selec
                         placeholder="Digite sua meta de zaps.."
                     />
                     {target === "" ? <button style={bStyle}>Iniciar Recall!</button>
-                    :<button onClick={()=> setDisplay(false)}>Iniciar Recall!</button>
+                    :<button onClick={()=>setDisplay(false)}>Iniciar Recall!</button>
                     }
                 </div>
         </div>    
